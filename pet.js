4c136ef964e3650a00a0c282e21af21b1ede9810
anupamagarg <dolly.singla@gmail.com>
2018-11-26 10:15:20 -0600

    initial commit

---
+++ pet.js
@@ -0,0 +1,16 @@
+/*Create an object that represents your pet.
+
+Name property with a string value.
+Species property with a string value.
+Nicknames property with an array value. Array contains strings.
+ Age property with an integer value*/
+
+ let myPet ={
+     name:"Snowy",
+     species: "cat",
+     nickNames : [
+         {CuteName:"cutie"},
+         {otherNickName: "pusy"}
+         ],
+     age: 2
+ }
